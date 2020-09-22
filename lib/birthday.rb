506567ad981f48fda5e148ce8f3de6566baf4b81
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 06:30:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,14 @@
 birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27	 }
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+}
 
 
 def happy_birthday(birthday_kids)
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
 end
 
 def age_appropriate_birthday(birthday_kids)
