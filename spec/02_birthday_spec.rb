906616dea07f67fb84fa150a55ec24524c0e4a1b
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 06:19:23 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -16,10 +16,10 @@ end
 ## BONUS ##
 ## Change "xit" to "it" in the test block to enable
 describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do	  it "only prints the birthday greeting if the birthday kid 12 or younger" do
-    birthday_kids = {	    birthday_kids = {
-      "Timmy" => 9,	      "Timmy" => 9,
-      "Sarah" => 6,	      "Sarah" => 6,
-
+  it "only prints the birthday greeting if the birthday kid 12 or younger" do	  it "only prints the birthday greeting if the birthday kid 12 or younger" do
+    birthday_kids = {
+      "Timmy" => 9,	
+      "Sarah" => 6,	
+}
   end
 end
