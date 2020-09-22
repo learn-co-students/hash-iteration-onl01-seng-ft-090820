30a4466fb94d39333d63f89b07c15794dce3dbbb
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-22 06:21:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,6 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
 end
 
 def age_appropriate_birthday(birthday_kids)
